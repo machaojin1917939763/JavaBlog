c42782ffef77edf97f6b1edf0aca2dc9aaffed44
machaojin <1917939763@qq.com>
2023-08-26 17:48:24 +0800

    更新文章

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/machaojin1917939763/",
 
   lang: "zh-CN",
   title: "JavaBlog",
