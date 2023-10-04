d32a4668a7c0ce3ec62efbb11915ea4aa8b5d71a
machaojin <1917939763@qq.com>
2023-10-04 22:40:07 +0800

    修改配置

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -7,10 +8,23 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "JavaBlog",
   description: "",
-
+ // 我们假定你在使用如下多语言
+ locales: {
+  "/": {
+    lang: "en-US",
+  },
+  "/zh/": {
+    lang: "zh-CN",
+  },
+},
 
 
   theme,
+  plugins: [
+    searchProPlugin({
+    
+    }),
+  ],
 
   // Enable it with pwa
   // shouldPrefetch: false,
