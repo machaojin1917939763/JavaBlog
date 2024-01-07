1adfc514d0c53c9488ac7e0eadf90b94ff30726e
machaojin <1917939763@qq.com>
2024-01-07 18:01:36 +0800

    添加ChatGPT

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -5,28 +5,23 @@ export default navbar([
   "/blog/",
   {
     text: "个人博客",
-    icon: "people",
     link:"http://120.48.18.228:8090/",
   },
   {
     text: "ChatGPT",
-    icon: "engine",
-    link:"http://120.48.18.228:3000/",
+    link:"http://120.48.18.228:8888/chat/share?shareId=u5keipyegqs6aspkmwfcntlu",
   },
   {
     text: "技术文档",
-    icon: "launch",
     prefix: "/guide/",
     children: [
       {
         text: "安装文档",
-        icon: "loop",
         prefix: "install/",
         children: [],
       },
       {
         text: "技术博客",
-        icon: "result",
         prefix: "book/",
         children: [],
       },
